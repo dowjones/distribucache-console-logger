77f1feba97c86d1b353c903d30e84f102c54ccd2
Yuriy Nemtsov <nemtsov@gmail.com>
2015-07-17 13:32:01 -0400

    add color to timestamp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import colors from 'colors/safe';
+
 const IS_ERROR_RE = /^error/;
 
 export default function logEvents(cache) {
   cache.onAny(function (...args) {
     const out = IS_ERROR_RE.test(this.event) ? 'stderr' : 'stdout';
     const timestamp = (new Date()).toISOString();
-    process[out].write(
-      `${timestamp} ${this.event} - ${args}\n`
-    );
+    process[out].write([
+      colors.grey(timestamp),
+      this.event,
+      colors.grey('-'),
+      args
+    ].join(' ') + '\n');
   });
 }
