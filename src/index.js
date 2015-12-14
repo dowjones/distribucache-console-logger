7e6f36455cf65ee258b59db208ba49b7705de2fa
Adam <adamrights@gmail.com>
2015-12-14 17:31:22 -0500

    use es6 =>

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ const IS_ERROR_RE = /^error/;
 export default function logEvents(cache, options = {}) {
   if (options.events) {
     const events = options.events;
-    events.forEach(function (event) {
-      cache.on(event, _logEvent);
-    });
+    events.forEach(event => cache.on(event, _logEvent));
   } else {
     cache.onAny(_logEvent);
   }
 
-  function _logEvent (...args){
+  function _logEvent (...args) {
     const out = IS_ERROR_RE.test(this.event) ? 'stderr' : 'stdout';
     const timestamp = (new Date()).toISOString();
     const log = [colors.grey(timestamp)];
