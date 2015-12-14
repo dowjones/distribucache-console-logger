d7a36c6b3a45e45d1f3fb430eff96869ba86ab52
Adam <adamrights@gmail.com>
2015-12-14 16:05:16 -0500

    Add ability to pass in options.events to specify what cache events to log

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import colors from 'colors/safe';
+import _forEach from 'lodash/collection/forEach';
 
 const IS_ERROR_RE = /^error/;
 
 export default function logEvents(cache, options = {}) {
-  cache.onAny(function (...args) {
-    const out = IS_ERROR_RE.test(this.event) ? 'stderr' : 'stdout';
-    const timestamp = (new Date()).toISOString();
-    const log = [colors.grey(timestamp)];
+  if (options.events) {
+    const events = options.events;
+    _forEach(events, function (event) {
+      cache.on(event, function (...args) {
+        const out = IS_ERROR_RE.test(this.event) ? 'stderr' : 'stdout';
+        const timestamp = (new Date()).toISOString();
+        const log = [colors.grey(timestamp)];
 
-    if (options.namespace) log.push(options.namespace);
-    log.push(colors.cyan(this.event), args);
+        if (options.namespace) log.push(options.namespace);
+        log.push(colors.cyan(this.event), args);
 
-    process[out].write(log.join(' ') + '\n');
-  });
+        process[out].write(log.join(' ') + '\n');
+      });
+    });
+  } else {
+    cache.onAny(function (...args) {
+      const out = IS_ERROR_RE.test(this.event) ? 'stderr' : 'stdout';
+      const timestamp = (new Date()).toISOString();
+      const log = [colors.grey(timestamp)];
+
+      if (options.namespace) log.push(options.namespace);
+      log.push(colors.cyan(this.event), args);
+
+      process[out].write(log.join(' ') + '\n');
+    });
+  }
 }
+
