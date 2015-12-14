7e9764ba58738230f78e19fd8db28dac86094c0b
Adam <adamrights@gmail.com>
2015-12-14 17:20:59 -0500

    Use native forEach and refactor common function call

--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
 import colors from 'colors/safe';
-import _forEach from 'lodash/collection/forEach';
 
 const IS_ERROR_RE = /^error/;
 
 export default function logEvents(cache, options = {}) {
   if (options.events) {
     const events = options.events;
-    _forEach(events, function (event) {
-      cache.on(event, function (...args) {
-        const out = IS_ERROR_RE.test(this.event) ? 'stderr' : 'stdout';
-        const timestamp = (new Date()).toISOString();
-        const log = [colors.grey(timestamp)];
-
-        if (options.namespace) log.push(options.namespace);
-        log.push(colors.cyan(this.event), args);
-
-        process[out].write(log.join(' ') + '\n');
-      });
+    events.forEach(function (event) {
+      cache.on(event, _logEvent);
     });
   } else {
-    cache.onAny(function (...args) {
-      const out = IS_ERROR_RE.test(this.event) ? 'stderr' : 'stdout';
-      const timestamp = (new Date()).toISOString();
-      const log = [colors.grey(timestamp)];
+    cache.onAny(_logEvent);
+  }
 
-      if (options.namespace) log.push(options.namespace);
-      log.push(colors.cyan(this.event), args);
-
-      process[out].write(log.join(' ') + '\n');
-    });
+  function _logEvent (...args){
+    const out = IS_ERROR_RE.test(this.event) ? 'stderr' : 'stdout';
+    const timestamp = (new Date()).toISOString();
+    const log = [colors.grey(timestamp)];
+    if (options.namespace) log.push(options.namespace);
+    log.push(colors.cyan(this.event), args);
+    process[out].write(log.join(' ') + '\n');
   }
 }
-
